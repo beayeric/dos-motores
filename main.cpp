65a71eeff923bacf1e9a23edf466529e67e1d0ba
beayeric <bye@iMac-de-ByE.local>
2020-05-25 00:51:12 +0200

    Intento 5

--- main.cpp
+++ main.cpp
@@ -1,36 +1,4 @@
-///int PUL=5; //define Pulse pin
-int DIR=6; //define Direction pin
-int ENA=5; //define Enable Pin
-void setup() {
-  pinMode (PUL, OUTPUT);
-  pinMode (DIR, OUTPUT);
-  pinMode (ENA, OUTPUT);
 
-}
-
-void loop() {
-  for (int i=0; i<6400; i++)    //Forward 5000 steps
-  {
-    digitalWrite(DIR,LOW);
-    digitalWrite(ENA,HIGH);
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(50);
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(50);
-  }
-  for (int i=0; i<6400; i++)   //Backward 5000 steps
-  {
-    digitalWrite(DIR,HIGH);
-    digitalWrite(ENA,HIGH);
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(50);
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(50);
-  }
-}////
-
-
---------------------------------------------------------
 // intento de codigo //
 
 
@@ -60,8 +28,6 @@ void setup() {
 void loop() {
   for (int i=0; i<6400; i++)    //Forward 5000 steps -- calcular tiempo
   {
-  
-    
     digitalWrite(PULM1,HIGH);
     delayMicroseconds(50);
     digitalWrite(PULM1,LOW);
@@ -70,8 +36,6 @@ void loop() {
 
     for (int i=0; i<6400; i++)    //Forward 5000 steps --- calcular tiempo
 
-    
-    
     digitalWrite(PULM2,HIGH);
     delayMicroseconds(50);
     digitalWrite(PULM2,LOW);
