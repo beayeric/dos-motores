6c5b2a988fe3e5ff02c2166906e9dc35922a75ff
beayeric <bye@iMac-de-ByE.local>
2020-05-25 18:26:19 +0200

    V6

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-
-// intento de codigo //
-
+#include <Arduino.h>
 
 int PULM1=5; //define Pulse pin Motor 1
 int DIRM1=6; //define Direction pin Motor 1
@@ -14,33 +12,33 @@ void setup() {
   pinMode (PULM1, OUTPUT);
   pinMode (DIRM1, OUTPUT);
   pinMode (ENAM1, OUTPUT);
-  digitalWrite(DIRM1,LOW);
-  digitalWrite(ENAM1,HIGH);
 
   pinMode (PULM2, OUTPUT);
   pinMode (DIRM2, OUTPUT);
   pinMode (ENAM2, OUTPUT);
-  digitalWrite(DIRM2,LOW);
-  digitalWrite(ENAM2,HIGH);
 
 }
-
+    
 void loop() {
   for (int i=0; i<6400; i++)    //Forward 5000 steps -- calcular tiempo
   {
+    digitalWrite(DIRM1,LOW);
+    digitalWrite(ENAM1,HIGH);
     digitalWrite(PULM1,HIGH);
     delayMicroseconds(50);
     digitalWrite(PULM1,LOW);
     delayMicroseconds(50);
-    }
+
+  }
 
     for (int i=0; i<6400; i++)    //Forward 5000 steps --- calcular tiempo
-
+{
+    digitalWrite(DIRM2,LOW);
+    digitalWrite(ENAM2,HIGH);
     digitalWrite(PULM2,HIGH);
     delayMicroseconds(50);
     digitalWrite(PULM2,LOW);
-    delayMicroseconds(50)
-
+    delayMicroseconds(50);
 
   }
   
